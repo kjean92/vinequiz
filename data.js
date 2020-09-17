a89b6ce04bb0184e551ae9dcc1c04ba093fb9720
Chey Rose Flammer <cheyroseflammer@gmail.com>
2020-09-17 11:12:44 -0700

    adding question data

---
+++ data.js
@@ -0,0 +1,107 @@
+const questions = [
+    // Cheys
+    {
+        quote: 'Road work ahead? Uh, yeah...',
+        answers: {
+            1: "Why wouldn't it?",
+            2: "That's what she said!",
+            3: "I sure hope it does",
+            4: "Lorem Ipsum Stuff"
+        },
+        correctAnswer: 3
+    },
+    {
+        quote: 'How much money do you have? 69 cents...Oh! You know what that means',
+        answers: {
+            1: "😏",
+            2: "I dont have enough money for chicken nuggets",
+            3: "ya nasty & you need jesus",
+            4: "Lorem Ipsum Stuff"
+        },
+        correctAnswer: 2
+    },
+    {
+        quote: 'Hurricane Katrina? More like hurricane',
+        answers: {
+            1: "vanilla",
+            2: "chinchilla",
+            3: "gorilla! Rest in peace Harambe",
+            4: "tortilla!"
+        },
+        correctAnswer: 4
+    },
+    {
+        quote: 'What are you doing!? My eye brows! Thats a big @$$ mirror.',
+        answers: {
+            1: "I have big @$$ eyebrows!",
+            2: "Dont judge me!",
+            3: "You know im blind Harold!",
+            4: "well you have a big @$$ @$$"
+        },
+        correctAnswer: 1
+    },
+    {
+        quote: 'So basically wot i was finking was *gets punched* ah fack',
+        answers: {
+            1: "I cant believe you've done this",
+            2: "Why would you do that?",
+            3: "Inconceivable!",
+            4: "You've just punched me!"
+        },
+        correctAnswer: 1
+    },
+
+    // Kaylenas Questions
+    {
+        quote: '       ',
+        answers: {
+            1: "           ",
+            2: "           ",
+            3: "           ",
+            4: "           ",
+        },
+        correctAnswer: num
+    },
+    {
+        quote: '       ',
+        answers: {
+            1: "           ",
+            2: "           ",
+            3: "           ",
+            4: "           ",
+        },
+        correctAnswer: num
+    },
+    {
+        quote: '       ',
+        answers: {
+            1: "           ",
+            2: "           ",
+            3: "           ",
+            4: "           ",
+        },
+        correctAnswer: num
+    },
+    {
+        quote: '       ',
+        answers: {
+            1: "           ",
+            2: "           ",
+            3: "           ",
+            4: "           ",
+        },
+        correctAnswer: num 
+    },
+    {
+        quote: '       ',
+        answers: {
+            1: "           ",
+            2: "           ",
+            3: "           ",
+            4: "           ",
+        },
+        correctAnswer: num
+    },
+
+// End questions
+]
